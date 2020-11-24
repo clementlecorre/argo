3496d792146d5f33c58cd198622b2db7c36ca8b5
Alex Collins <alex_collins@intuit.com>
2020-11-24 12:32:01 -0800

    tidy up resource editor
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- ui/src/app/shared/components/object-editor/object-editor.tsx
+++ ui/src/app/shared/components/object-editor/object-editor.tsx
@@ -1,23 +1,30 @@
 import {languages} from 'monaco-editor/esm/vs/editor/editor.api';
 import * as React from 'react';
-import {createRef, useEffect} from 'react';
+import {createRef, useEffect, useState} from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import {uiUrl} from '../../base';
+import {ScopedLocalStorage} from '../../scoped-local-storage';
 import {parse, stringify} from '../object-parser';
+import {ToggleButton} from '../toggle-button';
 
 interface Props<T> {
-    language?: string;
     type: string;
     value: T;
+    buttons?: React.ReactNode;
     onChange?: (value: T) => void;
     onError?: (error: Error) => void;
 }
 
+const defaultLang = 'yaml';
+
 export const ObjectEditor = <T extends any>(props: Props<T>) => {
-    const language = props.language || 'yaml';
+    const storage = new ScopedLocalStorage('object-editor');
+    const [lang, setLang] = useState<string>(storage.getItem('lang', defaultLang));
+
+    useEffect(() => storage.setItem('lang', lang, defaultLang), [lang]);
 
     useEffect(() => {
-        if (props.type && language === 'json') {
+        if (props.type && lang === 'json') {
             const uri = uiUrl('assets/openapi-spec/swagger.json');
             fetch(uri)
                 .then(res => res.json())
@@ -41,32 +48,42 @@ export const ObjectEditor = <T extends any>(props: Props<T>) => {
                 })
                 .catch(error => props.onError(error));
         }
-    }, [language]);
+    }, [lang]);
 
     const editor = createRef<MonacoEditor>();
 
     return (
-        <div onBlur={() => props.onChange && props.onChange(parse(editor.current.editor.getModel().getValue()))}>
-            <MonacoEditor
-                ref={editor}
-                key='editor'
-                value={stringify(props.value, language)}
-                language={language}
-                height='600px'
-                options={{
-                    readOnly: props.onChange === null,
-                    minimap: {enabled: false},
-                    lineNumbers: 'off',
-                    renderIndentGuides: false
-                }}
-            />
-            {props.onChange && (
-                <p>
-                    <i className='fa fa-info-circle' />{' '}
-                    {props.language === 'json' ? <>Full auto-completion enabled.</> : <>Basic completion for YAML. Switch to JSON for full auto-completion.</>}{' '}
-                    <a href='https://argoproj.github.io/argo/ide-setup/'>Learn how to get auto-completion in your IDE.</a>
-                </p>
-            )}
-        </div>
+        <>
+            <div>
+                <ToggleButton toggled={lang === 'yaml'} onToggle={() => setLang(lang === 'yaml' ? 'json' : 'yaml')}>
+                    YAML
+                </ToggleButton>
+                {props.buttons}
+            </div>
+            <div onBlur={() => props.onChange && props.onChange(parse(editor.current.editor.getModel().getValue()))}>
+                <MonacoEditor
+                    ref={editor}
+                    key='editor'
+                    value={stringify(props.value, lang)}
+                    language={lang}
+                    height='600px'
+                    options={{
+                        readOnly: props.onChange === null,
+                        minimap: {enabled: false},
+                        lineNumbers: 'off',
+                        renderIndentGuides: false
+                    }}
+                />
+            </div>
+            <p>
+                {props.onChange && (
+                    <>
+                        <i className='fa fa-info-circle' />
+                        {lang === 'json' ? <>Full auto-completion enabled.</> : <>Basic completion for YAML. Switch to JSON for full auto-completion.</>}
+                    </>
+                )}{' '}
+                <a href='https://argoproj.github.io/argo/ide-setup/'>Learn how to get auto-completion in your IDE.</a>
+            </p>
+        </>
     );
 };
